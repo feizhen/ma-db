430b3e84330bd448f62266775c7d81975030418a
李为 <123653079@qq.com>
2016-07-27 20:25:52 +0800

    写好了profile页面上传头像和管理员页面上传文件批量添加用户，目前文件保存在本地，以后需将路径改为服务器里对应路径

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,6 +1,60 @@
+var multer  = require('multer')
+var path = require('path');
+var app = require(path.resolve(__dirname, '../server'));
+var fs = require('fs');
+
 module.exports = function(server) {
-  // Install a `/` route that returns server status
+  //上传头像
+  var images_storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '../my-achievements/src/assets/images/upload_images')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + '.jpg')
+    }
+  });
+  var jsons_storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '../my-achievements/src/assets/images/upload_jsons')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + '.json')
+    }
+  });
+  var upload_image = multer({storage: images_storage});
+  var upload_json = multer({storage: jsons_storage});
   var router = server.loopback.Router();
+  var customer = app.models.Customer;
+  router.post('/upload-images', upload_image.single('upload-images'), function(req, res) {
+    // console.log('req.file: ', req.file);
+    // console.log('res.req.file: ', res.req.file);    
+    var image_path = req.file.path.slice(23)
+    // console.log('image_path: ', image_path)
+    customer.updateAll({username: 'zhangshan'}, {avatar: image_path});
+  });
+
+  //上传json文件
+  router.post('/upload-jsons', upload_json.single('upload-jsons'), function(req, res) {
+    // console.log('req.body: ', req.body);
+    // console.log('req.file: ', req.file);
+    // console.log('res.req.file: ', res.req.file);    
+    var json_path = res.req.file.path;
+    // console.log('json_path: ', json_path);
+    fs.readFile(json_path, 'utf8', function(err, data) {
+      var users = JSON.parse(data).user;
+      var length = JSON.parse(data).user.length;
+      // console.log('JSON.parse(data).user: ', JSON.parse(data).user);
+      // console.log('JSON.parse(data).user.length: ', JSON.parse(data).user.length);
+      // console.log('typeof(JSON.parse(data).user[0]): ', typeof(JSON.parse(data).user[0]));
+      for (var i = 0; i < length; i++)
+        customer.upsert(users[i], function() {
+          // console.log('无敌！！');
+        });
+    });
+  });
+
+  // Install a `/` route that returns server status
   router.get('/', server.loopback.status());
+
   server.use(router);
 };
