5734f69719c2d7c50b65d34f5e4923e15f853e64
李为 <123653079@qq.com>
2016-08-04 11:09:24 +0800

    弄好四部分上传 还有些小细节

--- server/boot/root.js
+++ server/boot/root.js
@@ -4,7 +4,6 @@ var app = require(path.resolve(__dirname, '../server'));
 var fs = require('fs');
 
 module.exports = function(server) {
-  //上传头像
   var images_storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, '../my-achievements/src/assets/images/upload_images')
@@ -21,38 +20,91 @@ module.exports = function(server) {
       cb(null, file.fieldname + '-' + Date.now() + '.json')
     }
   });
+  var homework_pictures_storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '../my-achievements/src/assets/images/upload_homework_pictures')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + '.jpg')
+    }
+  });
+  var homework_codes_storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '../my-achievements/src/assets/images/upload_homework_codes')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() + '.zip')
+    }
+  });
   var upload_image = multer({storage: images_storage});
   var upload_json = multer({storage: jsons_storage});
+  var upload_homework_picture = multer({storage: homework_pictures_storage});
+  var upload_homework_code = multer({storage: homework_codes_storage});
+
   var router = server.loopback.Router();
   var customer = app.models.Customer;
+  var commit = app.models.Commit;
+
+  //上传头像
   router.post('/upload-images', upload_image.single('upload-images'), function(req, res) {
-    // console.log('req.body: ', typeof(req.body.username));
-    // console.log('req.file: ', req.file);
-    // console.log('res.req.file: ', res.req.file);
-    res.send("hello");    
+    // res.send("hello");    
     var image_path = req.file.path.slice(23)
     var name = req.body.username
-    // console.log('image_path: ', image_path)
     customer.updateAll({username: name}, {avatar: image_path});
   });
 
   //上传json文件
   router.post('/upload-jsons', upload_json.single('upload-jsons'), function(req, res) {
-    // console.log('req.body: ', req.body);
-    // console.log('req.file: ', req.file);
-    // console.log('res.req.file: ', res.req.file);    
-    var json_path = res.req.file.path;
-    // console.log('json_path: ', json_path);
+    //现在是根据固定的json文件格式里来写的，json文件里有一个user数组，数组里面放的是各user的信息，到时候可根据需求改动。
+    var json_path = req.file.path;
     fs.readFile(json_path, 'utf8', function(err, data) {
       var users = JSON.parse(data).user;
       var length = JSON.parse(data).user.length;
-      // console.log('JSON.parse(data).user: ', JSON.parse(data).user);
-      // console.log('JSON.parse(data).user.length: ', JSON.parse(data).user.length);
-      // console.log('typeof(JSON.parse(data).user[0]): ', typeof(JSON.parse(data).user[0]));
       for (var i = 0; i < length; i++)
         customer.upsert(users[i], function() {
           // console.log('无敌！！');
         });
+    });
+  });
+
+  //上传作业截图
+  router.post('/upload-homework-pictures', upload_homework_picture.single('upload-homework-pictures'), function(req, res) {  
+    //因为现在数据模型里还没有存作业截图路径的属性，所以等数据模型改了之后再完善这一个函数，代码和上传作业的差不多。
+  });
+
+  //上传作业代码
+  router.post('/upload-homework-codes', upload_homework_code.single('upload-homework-codes'), function(req, res) { 
+    var id = req.body.homework_id;
+    var name = req.body.username;
+    var homework_codes_path = req.file.path;
+    var data = {
+      "github" : "",
+      "filePath" : "",
+      "description" : "",
+      "homework_id" : 0,
+      "title" : "",
+      "user" : {
+          "username" : "",
+          "fullname" : ""
+      },
+      "group" : 0,
+      "class" : 0,        
+    }
+    data.homework_id = id;
+    data.title = "my homework " + id;
+    customer.findOne({where: {username: name}}, function(err, result) {
+      data.user.username = result.username;
+      data.user.fullname = result.fullname;
+      data.group = result.group;
+      data.class = result.class;
+    });
+    commit.find({where: {homework_id: id, "user.username": name}}, function(err, result) {
+      if (result.length == 0) {
+        commit.upsert(data, function() {
+          console.log("upsert data success!!");
+        });
+      }
+      commit.updateAll({homework_id: id}, {filePath: homework_codes_path});
     });
   });
 
